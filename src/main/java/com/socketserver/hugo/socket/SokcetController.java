cb8afa21410d6cf35a5c0e42d0e9a2ca34f10eef
youngkwnag <youngkwang.hugo@gmail.com>
2024-06-19 18:26:27 +0900

    이전버전

--- src/main/java/com/socketserver/hugo/socket/SokcetController.java
+++ src/main/java/com/socketserver/hugo/socket/SokcetController.java
@@ -2,7 +2,6 @@ package com.socketserver.hugo.socket;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import com.socketserver.hugo.dto.RobotAnswerDTO;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,53 +12,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RestController
 public class SokcetController {
 
-    @SuppressWarnings("rawtypes")
     @Autowired
     private MyWebSocketHandler myHandler;
 
-    @SuppressWarnings("unchecked")
     @GetMapping("/hay")
-    public String hay(@RequestParam(name = "req",defaultValue = "") String param) {
+    public String hay(@RequestParam(name = "tq",defaultValue = "") String param) {
         System.out.println("리퀘스트쿼리"+param + param.isEmpty());
-
-        @SuppressWarnings("rawtypes")
-        RobotAnswerDTO robotAnswer = new RobotAnswerDTO("17");
         
-        robotAnswer.setSendMessage("hello world");
-      
-
+ 
+        String answer ="hello world";
         if(!param.isEmpty()){
-            switch (param) {
-                case "OK":
-                    robotAnswer.addJson("ACK", robotAnswer.getACK().ZERO);
-                    robotAnswer.addJson("STATUS", robotAnswer.getSTATUS().ORDER);
-                    robotAnswer.setSendMessage(robotAnswer.getJson());
-                    break;
-                case "FAIL":
-                    robotAnswer.addJson("STATUS", robotAnswer.getSTATUS().ORDER);
-                    robotAnswer.addJson("ACK", robotAnswer.getACK().ONE);
-                    robotAnswer.setSendMessage(robotAnswer.getJson());
-                    break;
-                case "FINISHE":
-                    robotAnswer.addJson("STATUS", robotAnswer.getSTATUS().FINISHED);
-                    robotAnswer.setSendMessage(robotAnswer.getJson());
-                    break;
-                case "TAKE":
-                    robotAnswer.addJson("STATUS", robotAnswer.getSTATUS().TAKEAWAY);
-                    robotAnswer.setSendMessage(robotAnswer.getJson());
-                    break;
-                default:
-                    System.out.println("디폴트");
-                    robotAnswer.setSendMessage(param);
-                    break;
-            }
+            answer = param;
+            
         }
    
         try {
-            myHandler.broadCast(robotAnswer.getSendMessage());
+            myHandler.broadCast(answer);
         
         } catch (Exception e) {
-            System.out.println("전송 실패");
+            // TODO: handle exception
             return "전송 실패";
         }
 
